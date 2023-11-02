1cd4b02cf950239016f2fa3ab81ebc22505b1a3a
federico <federico@codenmate.com>
2023-11-02 19:58:42 -0300

    Envio y recibo de comandos

--- app/src/main/java/com/example/myapplication/ConnectedActivity.java
+++ app/src/main/java/com/example/myapplication/ConnectedActivity.java
@@ -8,20 +8,32 @@ import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.UUID;
+
 
 public class ConnectedActivity extends AppCompatActivity {
 
     public Button veneno;
     public Button hayHormigas;
+    public Button salir;
+    public Button humedad;
     private static String address = null;
     private BluetoothAdapter btAdapter = null;
     private BluetoothSocket btSocket = null;
+    private ConnectedThread mConnectedThread;
+    final int handlerState = 0;
+    Handler bluetoothIn;
+    private StringBuilder recDataString = new StringBuilder();
+
+
     private static final UUID BTMODULEUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
     @SuppressLint("MissingPermission")
     @Override
@@ -39,10 +51,18 @@ public class ConnectedActivity extends AppCompatActivity {
         hayHormigas = findViewById(R.id.hayAnts);
         hayHormigas.setOnClickListener(botonesListeners);
 
+        salir = findViewById(R.id.salir);
+        salir.setOnClickListener(botonesListeners);
+
+        humedad = findViewById(R.id.hay_humedad);
+        humedad.setOnClickListener(botonesListeners);
+
         address= extras.getString("Direccion_Bluethoot");
 
         btAdapter = BluetoothAdapter.getDefaultAdapter();
         BluetoothDevice device = btAdapter.getRemoteDevice(address);
+
+        bluetoothIn = Handler_Msg_Hilo_Principal();
 
         //se realiza la conexion del Bluethoot crea y se conectandose a atraves de un socket
         try
@@ -69,7 +89,39 @@ public class ConnectedActivity extends AppCompatActivity {
                 //insert code to deal with this
             }
         }
+        mConnectedThread = new ConnectedThread(btSocket);
+        mConnectedThread.start();
+        mConnectedThread.write("I");
     }
+
+    private Handler Handler_Msg_Hilo_Principal ()
+    {
+        return new Handler() {
+            public void handleMessage(android.os.Message msg)
+            {
+                //si se recibio un msj del hilo secundario
+                if (msg.what == handlerState)
+                {
+                    //voy concatenando el msj
+                    String readMessage = (String) msg.obj;
+                    showToast(readMessage);
+                    recDataString.append(readMessage);
+                    int endOfLineIndex = recDataString.indexOf("\r\n");
+
+                    //cuando recibo toda una linea la muestro en el layout
+                    if (endOfLineIndex > 0)
+                    {
+                        String dataInPrint = recDataString.substring(0, endOfLineIndex);
+
+                        recDataString.delete(0, recDataString.length());
+                        showToast(dataInPrint);
+                    }
+                }
+            }
+        };
+
+    }
+
 
     @SuppressLint("MissingPermission")
     private BluetoothSocket createBluetoothSocket(BluetoothDevice device) throws IOException {
@@ -81,6 +133,11 @@ public class ConnectedActivity extends AppCompatActivity {
         Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
     }
 
+    //H hay Hormigas?
+    //R tirar veneno
+    //C consultar humedad
+    //I iniciar
+    //E salir
     View.OnClickListener botonesListeners = new View.OnClickListener()
     {
         @SuppressLint("MissingPermission")
@@ -89,22 +146,82 @@ public class ConnectedActivity extends AppCompatActivity {
         {
             if(v.getId()==R.id.tirar_ven)
             {
-                Intent intent;
-
-                //se genera un Intent para poder lanzar la activity principal
-                intent=new Intent(ConnectedActivity.this,TirarVen.class);
-
-                //Se le agrega al intent los parametros que se le quieren pasar a la activyt principal
-                //cuando se lanzado
-                intent.putExtra("textoOrigen","Hola Mundo");
-
-                //se inicia la activity principal
-                startActivity(intent);
+                mConnectedThread.write("R");
             }
-            else if(v.getId()==R.id.hay_ants)
+            else if(v.getId()==R.id.hayAnts)
             {
-
+                mConnectedThread.write("H");
+            }
+            else if(v.getId()==R.id.hay_humedad)
+            {
+                mConnectedThread.write("C");
+            }
+            else if(v.getId()==R.id.salir)
+            {
+                mConnectedThread.write("E");
             }
         }
     };
+
+    private class ConnectedThread extends Thread
+    {
+        private final InputStream mmInStream;
+        private final OutputStream mmOutStream;
+
+        //Constructor de la clase del hilo secundario
+        public ConnectedThread(BluetoothSocket socket)
+        {
+            InputStream tmpIn = null;
+            OutputStream tmpOut = null;
+
+            try
+            {
+                //Create I/O streams for connection
+                tmpIn = socket.getInputStream();
+                tmpOut = socket.getOutputStream();
+            } catch (IOException e) { }
+
+            mmInStream = tmpIn;
+            mmOutStream = tmpOut;
+        }
+
+        //metodo run del hilo, que va a entrar en una espera activa para recibir los msjs del HC05
+        public void run()
+        {
+            byte[] buffer = new byte[256];
+            int bytes;
+
+            //el hilo secundario se queda esperando mensajes del HC05
+            while (true)
+            {
+                try
+                {
+                    //se leen los datos del Bluethoot
+                    bytes = mmInStream.read(buffer);
+                    String readMessage = new String(buffer, 0, bytes);
+
+                    //se muestran en el layout de la activity, utilizando el handler del hilo
+                    // principal antes mencionado
+                    bluetoothIn.obtainMessage(handlerState, bytes, -1, readMessage).sendToTarget();
+                } catch (IOException e) {
+                    break;
+                }
+            }
+        }
+
+
+        //write method
+        public void write(String input) {
+            byte[] msgBuffer = input.getBytes();           //converts entered String into bytes
+            try {
+                mmOutStream.write(msgBuffer);                //write bytes over BT connection via outstream
+            } catch (IOException e) {
+                //if you cannot write, close the application
+                showToast("La conexion fallo");
+                finish();
+
+            }
+        }
+    }
+
 }
